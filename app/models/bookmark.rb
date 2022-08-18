5deb78e66ca9bf8d08ceb0d758be173e26c470ca
Marguerite Choyer <margueritechoyer@gmail.com>
2022-08-18 14:34:23 +0200

    Models ok

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,8 +1,9 @@
 class Bookmark < ApplicationRecord
-  belongs_to :movie, :list
+  belongs_to :movie, dependent: :destroy
+  belongs_to :list
 
-  validates: :comment, presence: true, length: { minimum: 6 }
-  validates: :movie, presence: true
-  validates: :list, presence: true
-  validates: :movie :list, uniqueness: true
+  validates :comment, presence: true, length:{ minimum: 6 }
+  validates :movie, presence: true
+  validates :list, presence: true
+  validates :movie, uniqueness: { scope: :list }
 end
